7d4dc43af186545e8f2f8cf5e75c2503ffdcee0a
regeciovad <regeciovad@gmail.com>
2015-05-30 13:37:04 +0200

    Copr_uploader methods (#9)

--- rpg/copr_uploader.py
+++ rpg/copr_uploader.py
@@ -1,2 +1,33 @@
+from rpg.command import Command
+from os.path import expanduser
+
 class CoprUploader:
-    pass
+    
+    def setup__copr_config(self, username, login, token):
+        config_path = expanduser('~') + '/.config/copr'
+        begin = '[copr-cli] \n'
+        username = 'username = ' + username + '\n'
+        login = 'login = ' + login + '\n'
+        token = 'token = ' + token + '\n'
+        copr_url = 'copr_url = http://copr-fe-dev.cloud.fedoraproject.org'
+        with open(config_path, 'w') as config_file:
+            config_file.write(begin + username + login + token + copr_url)
+                
+    
+    def create_copr(self, name, chroots, description='', instructions=''):
+        command = 'copr-cli create'
+        parameters = ''
+        for chroot in chroots:
+            parameters = parameters + ' --chroot ' + str(chroot)
+        if description:
+            parameters = parameters + ' --description \'' + description + '\''
+        if instructions:
+            parameters = parameters + ' --instructions \'' + instructions + '\''
+        parameters = parameters + ' ' + name
+        Command(command + parameters).execute()
+        
+    def build_copr(self, name, srpm_url):
+        command = 'copr-cli build '
+        parameters = name + ' ' + srpm_url
+        Command(command + parameters).execute()
+         
